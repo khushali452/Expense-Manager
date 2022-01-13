bfff52e7f95eb56e5bb90daf8c56d875f167a105
khushali452 <khushalivasani.ict19@gmail.com>
2022-01-13 18:04:00 +0530

    Version 1 with React Completed

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -1,5 +1,21 @@
 import React,{useContext} from 'react'
 import { GlobalContext } from '../context/GlobalState';
+
+function moneyFormatter(num) {
+    let p = num.toFixed(2).split('.');
+    return (
+      'Rs ' + (p[0].split('')[0]=== '-' ? '-' : '') +
+      p[0]
+        .split('')
+        .reverse()
+        .reduce(function (acc, num, i, orig) {
+          return num === '-' ? acc : num + (i && !(i % 3) ? ',' : '') + acc;
+        }, '') +
+      '.' +
+      p[1]
+    );
+  }
+
 export const Balance = () => {
     const {transactions}=  useContext(GlobalContext);
 
@@ -10,7 +26,7 @@ export const Balance = () => {
     return (
         <>
             <h4>Your Balance</h4>
-            <h1 >{total}</h1>
+            <h1 >{moneyFormatter(total)}</h1>
         </>
     )
 }
--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, {useContext} from 'react';
+import { GlobalContext } from '../context/GlobalState';
 
-export const Transaction = ({transaction}) => {
-    const sign =transaction.amount<0?'-':'+';
-    return (
+//Money formatter function
+function moneyFormatter(num) {
+  let p = num.toFixed(2).split('.');
+  return (
+    'Rs ' +
+    p[0]
+      .split('')
+      .reverse()
+      .reduce(function (acc, num, i, orig) {
+        return num === '-' ? acc : num + (i && !(i % 3) ? ',' : '') + acc;
+      }, '') +
+    '.' +
+    p[1]
+  );
+}
 
-        <li className={transaction.amount<0?'minus':'plus'}>
-                  {transaction.text}<span>{sign}Rs {Math.abs(transaction.amount)}</span><button className="delete-btn">x</button>
-              </li>
-    )
+export const Transaction = ({ transaction }) => {
+  const { deleteTransaction } = useContext(GlobalContext);
+
+  const sign = transaction.amount < 0 ? '-' : '+';
+
+  return (
+    <li className={transaction.amount < 0 ? 'minus' : 'plus'}>
+      {transaction.text} <span>{sign}{moneyFormatter(transaction.amount)}</span><button onClick={() => deleteTransaction(transaction.id)} className="delete-btn">x</button>
+    </li>
+  )
 }
